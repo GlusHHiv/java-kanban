0bda4fb38e0eaeedd4dc33f5a2ac048c096553a6
IvanGlush <poliantsevivan@gmail.com>
2024-03-08 18:22:43 +0300

    fix: codestyle

--- src/managers/history/HistoryManager.java
+++ src/managers/history/HistoryManager.java
@@ -1,12 +1,10 @@
 package managers.history;
 import model.Task;
 import java.util.List;
-import java.util.Map;
 
 public interface HistoryManager {
     void add(Task task);
     List<Task> getHistory();
-
     void remove(int id);
 
 }
