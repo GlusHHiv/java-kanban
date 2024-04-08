77c0b86c96d408e28d07a0e9c131ef8d2ee39337
IvanGlush <poliantsevivan@gmail.com>
2024-04-08 22:58:39 +0700

    fix: codeStyle

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,7 @@
-import managers.history.HistoryManager;
-import managers.history.InMemoryHistoryManager;
-import managers.task.FileBackedTaskManager;
-import managers.task.TaskManager;
-import model.Epic;
-import model.Status;
-import model.Subtask;
-import model.Task;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
     }
 }
