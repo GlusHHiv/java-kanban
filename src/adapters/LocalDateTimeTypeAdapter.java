56234892c6000ab8738016e126a40c04567c77ab
IvanGlush <poliantsevivan@gmail.com>
2024-06-26 23:01:47 +0200

    add: httpServer

--- src/adapters/LocalDateTimeTypeAdapter.java
+++ src/adapters/LocalDateTimeTypeAdapter.java
@@ -9,12 +9,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeTypeAdapter extends TypeAdapter<LocalDateTime> {
-        // задаём формат выходных данных: "dd-MM-yyyy"
         private static final  DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME; //ofPattern("yyyy-MM-dd:HH.mm");
 
         @Override
         public void write(final JsonWriter jsonWriter, final LocalDateTime localDate) throws IOException {
-            // приводим localDate к необходимому формату
             jsonWriter.value(localDate.format(dtf));
         }
 
